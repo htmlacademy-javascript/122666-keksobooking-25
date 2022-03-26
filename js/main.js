b6d0c600bf9b0bdac6eec9622449d9263fb52621
OxyGrimm <oxy.grimm@techork.com>
2022-03-26 18:30:32 +0200

    added map and slider initialization

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 
+import {createMap} from './createMap.js';
 import {mainFormHandler} from './mainForm.js';
 import {getOffers} from './get-offers.js';
 import {createSimilarOffers} from './create-similar-offers.js';
-import {activatePage, deactivatePage} from './page-state-controller.js';
+import {deactivatePage} from './page-state-controller.js';
 
 deactivatePage();
+createMap();
 mainFormHandler();
-setTimeout(activatePage, 2000);
 getOffers();
 createSimilarOffers();
